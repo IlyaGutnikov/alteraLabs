ea1989f71ada16be5235f083dd90d10eaf1c742c
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-05-01 00:03:53 +0300

    Финальный коммит перед заливкой на github.

--- ptsuits/lab3/test_proj1/countones.v
+++ ptsuits/lab3/test_proj1/countones.v
@@ -1,65 +1,25 @@
 module countones(din,result); 
 	
 	input [31:0] din; 
-	output reg [1:0] result; 
+	output reg [31:0] result; 
 	
-	reg [7:0] a;
-	reg [7:0] b;
-	reg [7:0] c;
-	reg [7:0] d;
-	
+	reg [31:0] a;
+	reg [31:0] b;
+
 	integer i;
-	integer t;
-	
-	integer a_int;
-	integer b_int;
-	integer c_int;
-	integer d_int;
-	
-	reg signed [7:0] temp [0:3];
-	
-	integer avr;
-	integer min;
- 
+
 	always @(*) 
 	begin 
 	
-		a = din[31:24];
-		b = din[23:16];
-		c = din[15:8];
-		d = din[7:0];
+		a = din[31:0];
 		
-		a_int = a;
-		b_int = b;
-		c_int = c;
-		d_int = d;
-		
-		//average
-		avr = (a_int + b_int + c_int + d_int)>>2;
-		//abs
-		temp[0] = avr - a_int;
-		temp[1] = avr - b_int;
-		temp[2] = avr - c_int;
-		temp[3] = avr - d_int;
-		
-		for (i = 0; i<4; i=i+1) 
+		for (i = 0; i<31; i=i+1) 
 		begin
-		if(temp[i] < 0) temp[i] = -temp[i];
+			b[31-i] = a[i];
 		end
 		
-		result = 0;
+		result = b;
 		
-		min = temp[3];
-		t = 0;
-		for (i = 0; i<4; i=i+1) 
-		begin
-		if(temp[i] < min)
-			begin
-			min = temp[i];
-			t = i;
-			end
-		end
-		result = t;
 	end 
 	
 endmodule
--- ptsuits/lab3/test_proj1/simulation/modelsim/countones_run_msim_rtl_verilog.do.bak11
+++ ptsuits/lab3/test_proj1/simulation/modelsim/countones_run_msim_rtl_verilog.do.bak11
@@ -5,9 +5,9 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -vlog01compat -work work +incdir+C:/git/alteralabs/ptsuits/lab3/test_proj1 {C:/git/alteralabs/ptsuits/lab3/test_proj1/countones.v}
+vlog -vlog01compat -work work +incdir+E:/alteraLabs/ptsuits/lab3/test_proj1 {E:/alteraLabs/ptsuits/lab3/test_proj1/countones.v}
 
-vlog -vlog01compat -work work +incdir+C:/git/alteralabs/ptsuits/lab3/test_proj1/simulation/modelsim {C:/git/alteralabs/ptsuits/lab3/test_proj1/simulation/modelsim/countones.vt}
+vlog -vlog01compat -work work +incdir+E:/alteraLabs/ptsuits/lab3/test_proj1/simulation/modelsim {E:/alteraLabs/ptsuits/lab3/test_proj1/simulation/modelsim/countones.vt}
 
 vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cycloneive_ver -L rtl_work -L work -voptargs="+acc"  countones_vlg_tst
 
